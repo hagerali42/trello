cab97911c7baf21611dd831551fe8665bcd16f14
Hager Ali <hageraliali42@gmail.com>
2023-07-26 17:36:31 +0300

    git commit "fix"

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import bootstrap from './src/index.router.js'
 const app = express()
 const port = 5000
 
-// sdfadfadsfasdfasd
-// sdfadfadsfasdfasd
-// sdfadfadsfasdfasd
+app.get('/', (req, res) =>{
+    res.json({message:'welcome '})
+})
 bootstrap(app , express)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
