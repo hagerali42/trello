184f4e0ab12a6ed3488035aa371a56226e444e48
Hager Ali <hageraliali42@gmail.com>
2023-07-27 00:04:34 +0300

    git commit "add"

--- DB/DBconection.js
+++ DB/DBconection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB=async()=>{
+    // connect
     return await mongoose.connect(process.env.DB_URL)
-
     .then(result=>{
         console.log('Database connected successfully');
     }).catch(err=>{
