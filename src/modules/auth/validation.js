073ebc0b8c08942b0f83a6a8a3d4d89fdd888031
Hager Ali <hageraliali42@gmail.com>
2023-07-29 23:36:16 +0300

    git commit "P"

--- src/modules/auth/validation.js
+++ src/modules/auth/validation.js
@@ -9,7 +9,7 @@ export const signup =Joi.object({
     phone: Joi.string().pattern(new RegExp(/^01[0125][0-9]{8}$/)),
     gender: Joi.string().required(),
     age: Joi.number().min(10).max(100).integer().positive(),  
-    isAdmin:Joi.boolean()  
+    isAdmain:Joi.boolean(), 
 }).required()
 
 export const login =Joi.object({
